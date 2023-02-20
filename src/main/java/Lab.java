49074ec26679b1e188865d12b1774adf2928db9e
mimi-curry <Mimi8400114@gmail.com>
2023-02-20 18:16:42 +0000

    commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -12,7 +12,7 @@ public class Lab {
      * All variables needed to solve the problem are provided by the method parameters - boolean bool, int x, int y.
      * This means that you can use the variables bool, x, and y freely, within the if statement, to solve the problem.
      *
-     * Additional Resource if needed: https://www.w3schools.com/java/java_conditions.asp
+     * Additional Resource if needed: 
      *
      * @param bool a true/false value that determines if x or y is to be returned.
      * @param x first number to be compared.
@@ -20,6 +20,18 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
-    }
-}
+         if (bool == true) {
+            return x;
+         }
+          else 
+            return y;
+         
+         
+         }
+       }
+     
+        
+    
+        
+    
+
